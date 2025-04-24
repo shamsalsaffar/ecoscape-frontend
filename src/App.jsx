402f62b009fa7c5165a8f5db41c047afb3346396
alex <nalleballeuwu@gmail.com>
2025-04-24 20:02:55 +0200

    footer finished, no clickable links yet due to no sites to redirect them to

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div>for now</div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
