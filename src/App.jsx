18398486657b9efaf15663feb63f9d218c28406b
Iasmina Alkashash <iasminakash@hotmail.com>
2025-04-28 12:27:27 +0200

    bring the header and banner to closest possible contour line , put a brightness filer on a header for more estetical appeareance

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-//   Navigate,
-// } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Listings from "./components/Listings";
-// import { useState } from "react";
+import { useState } from "react";
 import "./styles/header.css";
 import "./styles/home.css";
 import Footer from "./components/Footer";
@@ -17,58 +17,60 @@ import Helpcenter from "./components/Helpcenter";
 import HostPage from "./components/HostPage";
 import Banner from "./components/Banner";
 
-// function App() {
-//   const [listings, setListings] = useState([]);
+function App() {
+  const [listings, setListings] = useState([]);
 
-//   const fetchListings = () => {
-//     fetch("http://localhost:8080/api/listings")
-//       .then((res) => res.json())
-//       .then((data) => setListings(data))
-//       .catch((err) => console.error("Error fetching listings", err));
-//   };
+  const fetchListings = () => {
+    fetch("http://localhost:8080/api/listings")
+      .then((res) => res.json())
+      .then((data) => setListings(data))
+      .catch((err) => console.error("Error fetching listings", err));
+  };
 
-//   return (
-//     <Router>
-//       <div className="layout">
-//         <Header onFetch={fetchListings} />
-//         <Routes>
-//           {/* Omdirigera till startsidan */}
-//           <Route path="/" element={<Navigate to="/home" replace />} />
-//           {/* Startsidan - visar listings */}
-//           <Route
-//             path="/home"
-//             element={<Home listings={listings} onFetch={fetchListings} />}
-//           />
-//           {/* Reservsida: visa alla listor i enklare format  */}
-//           <Route
-//             path="/listings"
-//             element={<Listings listings={listings} onFetch={fetchListings} />}
-//           />
-//           // Vi skickar `fetchListings` till Signup-sidan så att när användaren
-//           registrerar sig framgångsrikt, // så hämtas och uppdateras listan med
-//           tillgängliga listings. Detta säkerställer att användaren // ser de
-//           senaste listings efter att ha registrerat sig.
-//           <Route
-//             path="/signup-page"
-//             element={<Signup onFetch={fetchListings} />}
-//           />
-//           <Route
-//             path="/login-page"
-//             element={<Login onFetch={fetchListings} />}
-//           />
-//           <Route
-//             path="/helpcenter-page"
-//             element={<Helpcenter onFetch={fetchListings} />}
-//           />
-//           <Route path="/host-page" element={<HostPage />} />
-//         </Routes>
-//       </div>
-//       <Footer />
-//     </Router>
-//   );
-// }
+  return (
+    <Router>
+      <div className="layout">
+        <Header onFetch={fetchListings} />
+        <Banner></Banner>
+        <Routes>
+          {/* Omdirigera till startsidan */}
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          {/* Startsidan - visar listings */}
+          <Route
+            path="/home"
+            element={<Home listings={listings} onFetch={fetchListings} />}
+          />
+          {/* Reservsida: visa alla listor i enklare format  */}
+          <Route
+            path="/listings"
+            element={<Listings listings={listings} onFetch={fetchListings} />}
+          />
+          // Vi skickar `fetchListings` till Signup-sidan så att när användaren
+          registrerar sig framgångsrikt, // så hämtas och uppdateras listan med
+          tillgängliga listings. Detta säkerställer att användaren // ser de
+          senaste listings efter att ha registrerat sig.
+          <Route
+            path="/signup-page"
+            element={<Signup onFetch={fetchListings} />}
+          />
+          <Route
+            path="/login-page"
+            element={<Login onFetch={fetchListings} />}
+          />
+          <Route
+            path="/helpcenter-page"
+            element={<Helpcenter onFetch={fetchListings} />}
+          />
+          <Route path="/host-page" element={<HostPage />} />
+        </Routes>
+      </div>
+      
+      <Footer />
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
 
 
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,7 +5,7 @@ import { PiUserListBold } from "react-icons/pi";
 const Button = ({onClick, text, children, style}) => {
     // props (text , color background, onclick, children (to icon support ))
     return(
-        <button className="button" onClick={onClick} style={style}>
+        <button className="header-button" onClick={onClick} style={style}>
         {children ? children : text}
       </button>// if dont argument children , you can argment text
 
