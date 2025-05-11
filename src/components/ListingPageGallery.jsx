9b84b8c30b13f66c285a6afb402bd6533c842a8d
Iasmina Alkashash <iasminakash@hotmail.com>
2025-05-11 12:37:34 +0200

    bring listing page gallery to a better view

--- src/components/ListingPageGallery.jsx
+++ src/components/ListingPageGallery.jsx
@@ -2,19 +2,22 @@ import "../styles/listingpagegallery.css";
 
 const ListingPageGallery = ({ mainImage, sideImages }) => {
   return (
+    <>
     <div className="listing-gallery-container">
       <div className="main-image-container">
      
         <img src={mainImage} className="image-1" alt="Main Listing" />
-        
+
+        </div>
         <div className="side-images-container">
          
           {sideImages.map((image, index) => (
             <img key={index} src={image} className={`image-${index + 2}`} alt={`Side Image ${index + 1}`} />
           ))}
-        </div>
+        
       </div>
     </div>
+    </>
   );
 };
 
