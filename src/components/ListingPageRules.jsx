ebdacf73697a76d368c688c0a62e5525194e2410
Iasmina Alkashash <iasminakash@hotmail.com>
2025-05-12 02:10:31 +0200

    styles listing page rules component, but further adjustments are needed

--- src/components/ListingPageRules.jsx
+++ src/components/ListingPageRules.jsx
@@ -7,123 +7,102 @@ import LoudMusicPolicy from "../icons/LoudMusicPolicy";
 import PetPolicy from "../icons/PetPolicy";
 import SelfCheckInPolicyIcon from "../icons/SelfCheckInPolicyIcon";
 import IdRequirement from "../icons/IdRequirement";
-
+import "../styles/listingpagerules.css";
 
 const ListingPageRules = ({ listing }) => {
-    console.log('isIdRequiredUponCheckin:', listing.rules.isIdRequiredUponCheckin);
-    console.log('listing.rules:', listing.rules);
-
-
   return (
     <>
       <div className="listing-page-rules">
         <h3 className="rules-heading">Rules</h3>
         {listing.rules.rulesText}
       </div>
+    
 
       <div className="rules-icons">
-        <div className="check-in-time-icon">
+        
+        <div className="check-in-time-icon rules-icon">
           <ClockIcon />
           <span>{listing.rules.checkInTime}Check In</span>
         </div>
 
-        <div className="check-out-time-icon">
+        <div className="check-out-time-icon rules-icon">
           <ClockIcon />
           <span>{listing.rules.checkOutTime}Check Out</span>
         </div>
 
         {listing.rules.quiteHoursStart != null && (
-  <div className="quiet-hours-start-icon">
-    <ClockIcon />
-    <span>{listing.rules.quiteHoursStart} Quiet Hours Start</span>
-  </div>
-)}
+          <div className="quiet-hours-start-icon rules-icon">
+            <ClockIcon />
+            <span>{listing.rules.quiteHoursStart} Quiet Hours Start</span>
+          </div>
+        )}
 
-{listing.rules.quiteQuiteHoursStop != null && (
-  <div className="quiet-hours-end-icon">
-    <ClockIcon />
-    <span>{listing.rules.quiteQuiteHoursStop} Quiet Hours End</span>
-  </div>
-)}
+        {listing.rules.quiteQuiteHoursStop != null && (
+          <div className="quiet-hours-end-icon rules-icon">
+            <ClockIcon />
+            <span>{listing.rules.quiteQuiteHoursStop} Quiet Hours End</span>
+          </div>
+        )}
 
+        {listing.rules.isSmokingAllowed != null && (
+          <div className="smoking-policy-icon rules-icon">
+            <SmokingPolicyIcon />
+            <span>Smoking</span>
+            {listing.rules.isSmokingAllowed ? <Permitted /> : <NotPermitted />}
+          </div>
+        )}
 
-   
-{listing.rules.isSmokingAllowed != null && (
-  <div className="smoking-policy-icon">
-    <SmokingPolicyIcon />
-    <span>Smoking</span>
-    {listing.rules.isSmokingAllowed ? (
-      <Permitted />
-    ) : (
-      <NotPermitted />
-    )}
-  </div>
-)}
+        {listing.rules.isPartyingAllowed != null && (
+          <div className="party-policy-icon rules-icon">
+            <PartyPolicyIcon />
+            <span>Party</span>
+            {listing.rules.isPartyingAllowed ? <Permitted /> : <NotPermitted />}
+          </div>
+        )}
 
-{listing.rules.isPartyingAllowed != null && (
-  <div className="party-policy-icon">
-    <PartyPolicyIcon />
-    <span>Party</span>
-    {listing.rules.isPartyingAllowed ? (
-      <Permitted />
-    ) : (
-      <NotPermitted />
-    )}
-  </div>
-)}
+        {listing.rules.isLoudMusicAllowed != null && (
+          <div className="loud-music-policy-icon rules-icon">
+            <LoudMusicPolicy />
+            <span>Loud Music</span>
+            {listing.rules.isLoudMusicAllowed ? (
+              <Permitted />
+            ) : (
+              <NotPermitted />
+            )}
+          </div>
+        )}
 
-{listing.rules.isLoudMusicAllowed != null && (
-  <div className="loud-music-policy-icon">
-    <LoudMusicPolicy />
-    <span>Loud Music</span>
-    {listing.rules.isLoudMusicAllowed ? (
-      <Permitted />
-    ) : (
-      <NotPermitted />
-    )}
-  </div>
-)}
+        {listing.rules.isPetsAllowed != null && (
+          <div className="pet-policy-icon rules-icon">
+            <PetPolicy />
+            <span>Pets Allowed</span>
+            {listing.rules.isPetsAllowed ? <Permitted /> : <NotPermitted />}
+          </div>
+        )}
 
-{listing.rules.isPetsAllowed != null && (
-  <div className="pet-policy-icon">
-    <PetPolicy />
-    <span>Pets Allowed</span>
-    {listing.rules.isPetsAllowed ? (
-      <Permitted />
-    ) : (
-      <NotPermitted />
-    )}
-  </div>
-)}
+        {listing.rules.isSelfCheckingPossible != null && (
+          <div className="self-checkin-policy-icon rules-icon">
+            <SelfCheckInPolicyIcon />
+            <span>Self Check-in</span>
+            {listing.rules.isSelfCheckingPossible ? (
+              <Permitted />
+            ) : (
+              <NotPermitted />
+            )}
+          </div>
+        )}
 
-{listing.rules.isSelfCheckingPossible != null && (
-  <div className="self-checkin-policy-icon">
-    <SelfCheckInPolicyIcon />
-    <span>Self Check-in</span>
-    {listing.rules.isSelfCheckingPossible ? (
-      <Permitted />
-    ) : (
-      <NotPermitted />
-    )}
-  </div>
-)}
-
-
-{listing.rules?.isIdRequiredUponCheckIn !== undefined && ( //behövde kolla om defined, annars ville inte visas
-  <div className="id-requirement-icon">
-    <IdRequirement />
-    <span>Id required</span>
-    {listing.rules.isIdRequiredUponCheckIn ? (
-      <Permitted />
-    ) : (
-      <NotPermitted />
-    )}
-  </div>
-)}
-
-
-
-
+        {listing.rules?.isIdRequiredUponCheckIn !== undefined && ( //behövde kolla om defined, annars ville inte visas
+          <div className="id-requirement-icon rules-icon">
+            <IdRequirement />
+            <span>Id required</span>
+            {listing.rules.isIdRequiredUponCheckIn ? (
+              <Permitted />
+            ) : (
+              <NotPermitted />
+            )}
+          </div>
+        )}
       </div>
     </>
   );
