4cb237471adbedc095e06efac55b63c34fb7c603
alex <nalleballeuwu@gmail.com>
2025-05-15 00:03:21 +0200

    aligned everything according to hi-fi for now

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,6 +3,7 @@ import ProfileBackground from "../components/ProfileBackground";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getUserById } from "../api/userService";
+import Button from "../components/Button";
 
 const Profile = () => {
   const { userId } = useParams();
@@ -26,13 +27,42 @@ const Profile = () => {
   return (
     <div className="profile-page">
       <ProfileBackground minWidth="1250px" minHeight="550px">
-        <div className="profile-image">
-          <img src={photoUrl} alt="user-photo" />
+        <div className="profile-content">
+          <div className="profile-image-container">
+            <img className="profile-image" src={photoUrl} alt="user-photo" />
+            <h5>Upload a Photo</h5>
+          </div>
+          <div className="profile-details">
+            <div className="profile-title">
+              <h2>
+                Hello, {user?.firstName} {user?.lastName}
+              </h2>
+            </div>
+            <div className="profile-about">
+              <h3>Bio</h3>
+              {user?.bio}
+            </div>
+            <div className="profile-birth">
+              <h3>Birth date</h3>
+              {user?.birthDate}
+            </div>
+            <div className="profile-number">
+              <h3>Contact phone number</h3>
+              {user?.contactPhoneNumber}
+            </div>
+            <div className="profile-email">
+              <h3>Contact email</h3>
+              {user?.contactEmail}
+            </div>
+            <Button
+              className="profile-button"
+              text="Edit profile"
+              type="button"
+              variant="auth"
+              onClick={() => navigate("/nothing-yet")}
+            />
+          </div>
         </div>
-        <div className="profile-about">{user?.bio}</div>
-        <div className="profile-birth">{user?.birthDate}</div>
-        <div className="profile-number">{user?.contactPhoneNumber}</div>
-        <div className="profile-email">{user?.contactEmail}</div>
       </ProfileBackground>
     </div>
   );
