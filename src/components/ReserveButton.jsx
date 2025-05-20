e6ce16eb92e20aadd95b21d96966583967afad76
Iasmina Alkashash <iasminakash@hotmail.com>
2025-05-20 22:32:51 +0200

    implementing category search functionality

--- src/components/ReserveButton.jsx
+++ src/components/ReserveButton.jsx
@@ -1,5 +1,4 @@
 import { useNavigate } from "react-router-dom";
-import React, { useState, useEffect } from "react";
 import "../styles/ReserveButton.css";
 import { useBooking } from "../contexts/BookingContext";
 
